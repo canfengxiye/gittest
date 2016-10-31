3c9bc66b8ecf1192e437578b6625c990ab67807f
canfengxiye <417547426@qq.com>
2016-10-31 20:10:18 +0800

    first commit

---
+++ type.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    cout << "int size : " << sizeof(int) <<endl;
+    cout << "long size : " << sizeof(long) <<endl;
+    cout << "long long size : " << sizeof(long long) <<endl;
+    cout << "double size : " << sizeof(double) <<endl;
+    cout << "float size : " << sizeof(float) <<endl;
+
+    return 0;
+}
